e4698fa9bfa0ca453d19de6d4260718f46d3679c
Radhika Gupta <radhikagupta@Radhikas-MacBook-Pro.local>
2024-02-21 19:27:30 +0530

    first commit

---
+++ src/Form.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+
+const Form = () => {
+    const [firstname,setFirstname]=useState("")
+    const [lastname,setLastname]=useState("")
+
+    const [fullname,setFullname]=useState("")
+
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+       setFullname(" Full name: "+firstname+" "+lastname)
+    }
+  return (
+    <div>
+       <form onSubmit={handleSubmit}>
+        <h1>Full Name Display</h1>
+        <br/>
+      First Name:<input type="text" value={firstname} onChange={(e)=> setFirstname(e.target.value)} required/>
+      <br/>
+      Last Name:<input type="text" value={lastname} onChange={(e)=> setLastname(e.target.value)} required/>
+      <br/>
+      <button type="submit" >Submit</button>
+      <br/>
+    {fullname}
+      </form>
+    </div>
+  )
+}
+
+export default Form
